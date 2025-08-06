c8e741ab589b67bb88e91e6579d6555cd7fb83e5
vuthanhtung2412 <vuthanhtung2016hn@gmail.com>
2025-08-06 09:37:16 +0200

    Move initial loading to a more appropriate position

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { useTranslation } from "react-i18next";
 import "./localization/i18n";
@@ -8,39 +8,16 @@ import { RouterProvider } from "@tanstack/react-router";
 import { BACKEND_URL } from "./const";
 import { Toaster } from "@/components/ui/sonner";
 import { OpenAPI } from "@/client/core/OpenAPI";
-import { syncThemeWithLocal } from "@/helpers/theme_helpers";
 
 export default function App() {
-  const { i18n } = useTranslation();
-  const [isHealthy, setIsHealthy] = useState(false);
-
-  useEffect(() => {
-    syncThemeWithLocal();
-    updateAppLanguage(i18n);
-
-    const checkHealth = async () => {
-      try {
-        const response = await fetch(`${BACKEND_URL}/health`);
-        if (response.status === 200) {
-          setIsHealthy(true);
-        } else {
-          throw new Error("Service not healthy");
-        }
-      } catch (error) {
-        console.log("Health check failed, retrying...");
-        setTimeout(checkHealth, 3000); // Retry after 1 second
-      }
-    };
-
-    checkHealth();
-  }, [i18n]);
-
-  if (!isHealthy) {
-    return <div>Loading...</div>; // Show a loading screen until the service is healthy
-  }
-
   // OpenAPI config
   OpenAPI.BASE = BACKEND_URL;
+
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    updateAppLanguage(i18n);
+  }, [i18n]);
 
   return <>
     <RouterProvider router={router} />
